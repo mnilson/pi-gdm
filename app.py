844cc26a35deb06f470efc9ac1a7933bb36568bd
Michael Nilson <michael.scott.nilson@gmail.com>
2018-10-15 03:31:23 +0000

    Adding some dummy GPIO code to test gpiozero

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from gpiozero import LED
 app = Flask(__name__)
+
+#led = LED(17)
+
+def flash_led(num):
+	for i in range(num):
+		led.on()
+		sleep(1)
+		led.off()
+		sleep(1)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, World!'
+	flash_led(5)
+	return 'Hello, World'
 
 if __name__ == '__main__':
 	app.run()
